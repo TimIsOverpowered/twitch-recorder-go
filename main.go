c637c808d0e7a9efd3cb69b0f7ce3fd245e6fc23
TimIsOverpowered <admin@angelthump.com>
2021-09-17 09:25:52 -0500

    add rw timeout for stuck m3u8

--- main.go
+++ main.go
@@ -886,7 +886,7 @@ func record(m3u8 string, channel string) error {
 	} else {
 		//use ffmpeg
 		log.Printf("[%s] Executing ffmpeg: %s", channel, "ffmpeg -y -i "+m3u8+" -c copy -copyts -start_at_zero -bsf:a aac_adtstoasc -f mp4 "+path+fileName)
-		cmd := exec.Command("ffmpeg", "-y", "-i", m3u8, "-c", "copy", "-copyts", "-start_at_zero", "-bsf:a", "aac_adtstoasc", "-f", "mp4", path+fileName)
+		cmd := exec.Command("ffmpeg", "-y", "-rw_timeout", "3000000", "-i", m3u8, "-c", "copy", "-copyts", "-start_at_zero", "-bsf:a", "aac_adtstoasc", "-f", "mp4", path+fileName)
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 		log.Printf("[%s] Finished downloading.. Saved at: %s", channel, path+fileName)
