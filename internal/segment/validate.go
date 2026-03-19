920a69ed90601f6808705c8494e953c61f35ab30
TimIsOverpowered <admin@angelthump.com>
2026-03-19 07:01:11 -0500

    Add config validation
    - Create internal/segment/validate.go
    - Validate vod_directory exists and is writable
    - Check channels list is not empty
    - Validate channel names (length, characters)

---
+++ internal/segment/validate.go
@@ -0,0 +1,56 @@
+package segment
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+func ValidateConfig(vodDirectory string, channels []string) error {
+	if vodDirectory == "" {
+		return errors.New("vod_directory is required")
+	}
+
+	if _, err := os.Stat(vodDirectory); os.IsNotExist(err) {
+		if err := os.MkdirAll(vodDirectory, 0755); err != nil {
+			return fmt.Errorf("cannot create vod_directory: %w", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("error accessing vod_directory: %w", err)
+	}
+
+	info, err := os.Stat(vodDirectory)
+	if err != nil {
+		return fmt.Errorf("cannot stat vod_directory: %w", err)
+	}
+
+	if !info.IsDir() {
+		return errors.New("vod_directory must be a directory")
+	}
+
+	testFile := vodDirectory + "/.write_test"
+	if err := os.WriteFile(testFile, []byte(""), 0644); err != nil {
+		return fmt.Errorf("vod_directory is not writable: %w", err)
+	}
+	os.Remove(testFile)
+
+	if len(channels) == 0 {
+		return errors.New("at least one channel must be specified")
+	}
+
+	for _, channel := range channels {
+		if channel == "" {
+			return errors.New("channel name cannot be empty")
+		}
+		if len(channel) > 25 {
+			return fmt.Errorf("channel name '%s' exceeds maximum length of 25 characters", channel)
+		}
+		for _, r := range channel {
+			if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') && (r < '0' || r > '9') && r != '_' {
+				return fmt.Errorf("channel name '%s' contains invalid characters", channel)
+			}
+		}
+	}
+
+	return nil
+}
